51e59f93dfd4c704ce9408ede348f9a0f9e86b76
arunsingh28 <arunsingh28aug.as@gmail.com>
2023-08-11 00:51:09 +0530

    input padding

--- components/website/InputWithButton.tsx
+++ components/website/InputWithButton.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const InputWithButton = () => {
     return (
         <div className='relative my-5 font-inter hero-input-text w-6/7 lg:w-full'>
-            <input type="text" className='w-full py-5 rounded-full px-3 text-black placeholder:font-bold input-p' placeholder='Enter your email to start' />
-            <button className='font-inter font-semibold px-4 bg-btnYellow py-4 rounded-full text-black absolute top-1 right-1 tracking-tighter input-btn-inside'>Claim your shop</button>
+            <input type="text" className='w-full input-p rounded-full text-black placeholder:font-bold' placeholder='Enter your email to start' />
+            <button className='font-inter font-semibold bg-btnYellow rounded-full text-black absolute top-1 right-1 tracking-tighter input-btn-inside'>Claim your shop</button>
         </div>
     )
 }
